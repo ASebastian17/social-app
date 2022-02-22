b212ed961f981b0d644d9e48f480a8f703bb00d6
Sebastian <armasebas17@hotmail.com>
2022-02-22 13:37:12 -0500

    finished post routes and controllers

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -68,6 +68,51 @@ export const remove = async (req, res) => {
     }
 };
 
+export const getPostsByUser = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const posts = await Post.find({ creator: userId });
+
+        res.status(200).json(posts);
+    } catch (e) {
+        res.status(400).json({ message: 'Invalid user id' });
+    }
+};
+
+export const likePost = async (req, res) => {
+    const { postId } = req.params;
+
+    try {
+        let post = await Post.findById(postId);
+        const index = post.likes.findIndex((userId) => userId === String(req.userId));
+
+        if (index === -1) {
+            post.likes.push(req.userId);
+        } else {
+            post.likes = post.likes.filter((userId) => userId !== String(req.userId));
+        }
+
+        post = await Post.findByIdAndUpdate(postId, post, { new: true });
+
+        res.status(201).json(post);
+    } catch (e) {
+        res.status(400).json({ message: 'Invalid post id' });
+    }
+};
+
+export const getPostComments = async (req, res) => {
+    const { postId } = req.params;
+
+    try {
+        const post = await Post.findById(postId);
+
+        res.status(200).json(post.comments);
+    } catch (e) {
+        res.status(400).json({ message: 'Invalid post id' });
+    }
+};
+
 export const addComment = async (req, res) => {
     const { postId } = req.params;
 
@@ -81,4 +126,26 @@ export const addComment = async (req, res) => {
     } catch (e) {
         res.status(400).json({ message: 'Invalid post id' });
     }
+};
+
+export const getComment = async (req, res) => {
+    const { postId, commentId } = req.params;
+
+    try {
+        const post = await Post.findById(postId);
+
+        const comment = post.comments.filter((comment) => comment._id.toString() === commentId);
+
+        res.status(200).json(comment);
+    } catch (e) {
+        res.status(400).json({ message: 'Invalid post id' });
+    }
+};
+
+export const getPostsByTag = async (req, res) => {
+    const tag = new RegExp(`^${req.params.tag}$`, 'i');
+
+    const posts = await Post.find({ tags: { $in: tag } });
+
+    res.status(200).json(posts);
 };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -9,13 +9,25 @@ router.route('/posts')
     .get(isAuthenticated, postCtrl.getAllPosts)
     .post(isAuthenticated, postCtrl.create);
     
-    router.route('/posts/:postId')
+router.route('/posts/:postId')
     .get(isAuthenticated, postCtrl.getPostById)
     .put(isAuthenticated, postCtrl.update)
     .delete(isAuthenticated, postCtrl.remove);
+
+router.route('/posts/user/:userId')
+    .get(isAuthenticated, postCtrl.getPostsByUser);
+    
+router.route('/posts/:postId/like')
+    .put(isAuthenticated, postCtrl.likePost);
     
 router.route('/posts/:postId/comment')
+    .get(isAuthenticated, postCtrl.getPostComments)
     .post(isAuthenticated, postCtrl.addComment);
-    
+
+router.route('/posts/:postId/comment/:commentId')
+    .get(isAuthenticated, postCtrl.getComment);
+
+router.route('/posts/tag/:tag')
+    .get(isAuthenticated, postCtrl.getPostsByTag);
     
 export default router;
